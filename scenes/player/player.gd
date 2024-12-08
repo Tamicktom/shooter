e5ade25c7b9b35486abb6a54f90ecb17a30616ee
Tamicktom <rickpokemon38@gmail.com>
2024-12-08 11:34:22 -0300

    Update player.gd

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -13,11 +13,5 @@ func _process(delta: float) -> void:
 	pass
 
 func movement(delta: float) -> void:
-	if Input.is_action_pressed("left"):
-		position.x -= SPEED * delta;
-	if Input.is_action_pressed("right"):
-		position.x += SPEED * delta;
-	if Input.is_action_pressed("up"):
-		position.y -= SPEED * delta;
-	if Input.is_action_pressed("down"):
-		position.y += SPEED * delta;
+	var direction = Input.get_vector("left","right","up","down");
+	position += direction * SPEED * delta;
