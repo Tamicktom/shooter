86eadd8344f8ec264dcf85ee92ef71c2bbf64738
Tamicktom <rickpokemon38@gmail.com>
2024-12-08 16:59:15 -0300

    basic signals

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -9,3 +9,13 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("Entered");
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	print("Exited");
+	pass # Replace with function body.
