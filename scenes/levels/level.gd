28d3220dd274a02096bbaa2f809e6779a6f39a88
Tamicktom <rickpokemon38@gmail.com>
2024-12-08 12:33:22 -0300

    delete some stuff

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
-const SPEED = 250;
-var player_position = Vector2(0,0);
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("level node is ready");	
@@ -11,6 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	player_position = $Player.position;
-	$Logo.update_position(player_position);
 	pass
