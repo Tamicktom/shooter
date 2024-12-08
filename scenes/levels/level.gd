e1c2482632f2c6dc18764e232f3f3e513fa3dc3e
Tamicktom <rickpokemon38@gmail.com>
2024-12-08 11:55:07 -0300

    testing nodes

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,25 +1,16 @@
 extends Node2D
 
 const SPEED = 250;
-
-var test_array: Array[String] = ["Test", "Banana", "Stuff"];
+var player_position = Vector2(0,0);
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("level node is ready");
-	$Logo.rotation_degrees = 90;
-	
-	for i in test_array:
-		print(i);
-	
+	print("level node is ready");	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Logo.rotation_degrees += 100 * delta;
-	
-	if $Logo.rotation_degrees > 180:
-		$Logo.rotation_degrees = 0;
-	
+	player_position = $Player.position;
+	$Logo.update_position(player_position);
 	pass
--- scenes/logo.gd
+++ scenes/logo.gd
@@ -1,26 +1,16 @@
 extends Sprite2D
 
-var pos: Vector2 = Vector2.ZERO;
-const SPEED: int = 500;
-var test_scale: float = 1;
-
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pos = Vector2(300,200);
-	position = pos;
-	
-	test_scale = 2;
-	
-	#var test_rotation = 45;
-	#rotation_degrees = test_rotation
-	
+func _ready() -> void:	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#test_scale += 0.01
-	position = pos;
-	#rotation_degrees += SPEED;
-	#scale = Vector2(test_scale, test_scale);
 	pass
+
+func update_position(pos:Vector2)->void:
+	var tmp_pos = pos;
+	tmp_pos.y += 50;
+	position = tmp_pos;
+	
