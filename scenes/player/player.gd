9bf6e64c3c3eebbc2dc5bb5457bf677cd1d56c29
Tamicktom <rickpokemon38@gmail.com>
2024-12-08 16:51:59 -0300

    create laser and granader cowdowns

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 const SPEED:int = 350;
 
+var can_laser: bool = true;
+var can_granade: bool = true;
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,14 +15,34 @@ func _process(_delta: float) -> void:
 	movement();
 	
 	# Laser shooting input
-	if Input.is_action_pressed("primaty-action"):
+	if Input.is_action_pressed("primaty-action") and can_laser:
 		print("Shoot");
-	
-	if Input.is_action_pressed("secondary-action"):
+		can_laser = false;
+		$LaserTimer.start();
+
+	# Granade shooting input	
+	if Input.is_action_pressed("secondary-action") and can_granade:
 		print("Granade");
+		can_granade = false;
+		$GranadeTimer.start();
+		
 	pass
 
 func movement() -> void:
 	var direction = Input.get_vector("left","right","up","down");
 	velocity = direction * SPEED;
 	move_and_slide();
+
+
+func _on_granade_timer_timeout() -> void:
+	print("granade timer timeout");
+	$GranadeTimer.stop();
+	can_granade = true;
+	pass # Replace with function body.
+
+
+func _on_laser_timer_timeout() -> void:
+	print("laser timer timeout");
+	$LaserTimer.stop();
+	can_laser = true;
+	pass # Replace with function body.
