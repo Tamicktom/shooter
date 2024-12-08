edcb0cb2236c459c948a0a949b071db3756e42ce
Tamicktom <rickpokemon38@gmail.com>
2024-12-08 17:27:06 -0300

    add timers for skills

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -16,17 +16,13 @@ func _process(_delta: float) -> void:
 	
 	# Laser shooting input
 	if Input.is_action_pressed("primaty-action") and can_laser:
-		print("Shoot");
 		can_laser = false;
 		$LaserTimer.start();
 
 	# Granade shooting input	
 	if Input.is_action_pressed("secondary-action") and can_granade:
-		print("Granade");
 		can_granade = false;
 		$GranadeTimer.start();
-		
-	pass
 
 func movement() -> void:
 	var direction = Input.get_vector("left","right","up","down");
@@ -35,14 +31,8 @@ func movement() -> void:
 
 
 func _on_granade_timer_timeout() -> void:
-	print("granade timer timeout");
-	$GranadeTimer.stop();
 	can_granade = true;
-	pass # Replace with function body.
 
 
 func _on_laser_timer_timeout() -> void:
-	print("laser timer timeout");
-	$LaserTimer.stop();
 	can_laser = true;
-	pass # Replace with function body.
