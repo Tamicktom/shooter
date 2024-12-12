fd9ee13d1af91f1ab0578c69a0bd605242e11c30
Tamicktom <rickpokemon38@gmail.com>
2024-12-11 21:05:40 -0300

    small update to the granade

--- scenes/granade.gd
+++ scenes/granade.gd
@@ -1,13 +1,12 @@
-extends Area2D
+extends RigidBody2D;
 
-@export var SPEED: int = 1000;
-const ALIVE_TIME: int = 3; # Seconds
+@export_range(100, 2000, 100) var SPEED: int = 1000;
+@export_range(1, 10, 1) var ALIVE_TIME: int = 3; # Seconds
 
 var direction: Vector2 = Vector2.UP;
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
   $Timer.wait_time = ALIVE_TIME; # Reset the timer
-  position += direction * SPEED * delta;
 
 func _on_timer_timeout() -> void:
   queue_free();
--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Node2D;
 
 var laser_scene: PackedScene = preload("res://scenes/laser.tscn");
 var granade_scene: PackedScene = preload("res://scenes/granade.tscn");
@@ -6,33 +6,23 @@ var granade_scene: PackedScene = preload("res://scenes/granade.tscn");
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("level node is ready");
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	pass;
 
-
-func _on_gate_player_entered_gate(body) -> void:
-	pass # Replace with function body.
-
+func _on_gate_player_entered_gate(_body: Node2D) -> void:
+	pass; # Replace with function body.
 
 func _on_player_granade_input(pos: Vector2) -> void:
-	var granade = granade_scene.instantiate();
-
+	var granade: RigidBody2D = granade_scene.instantiate();
 	granade.position = pos;
-
+	granade.linear_velocity = Vector2.UP * 100;
 	$Projectiles.add_child(granade);
-
 
 func _on_player_laser_input(pos: Vector2) -> void:
 	# Spawn the laser
-	var laser = laser_scene.instantiate();
+	var laser: Area2D = laser_scene.instantiate();
 	# Update the laser position
 	laser.position = pos;
-
-	# Move the laser
 	$Projectiles.add_child(laser);
-		
-	# Add the laser instance to a Node2D
