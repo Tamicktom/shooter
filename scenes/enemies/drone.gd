51a52335217b01035a277f98e1f8632fd81c7afb
Tamicktom <rickpokemon38@gmail.com>
2024-12-23 16:30:31 -0300

    ok

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -1,6 +1,11 @@
-extends CharacterBody2D
+extends CharacterBody2D;
+
+var is_enemy:bool = true;
 
 func _process(_delta: float) -> void:
 	var direction = Vector2.RIGHT;
 	velocity = direction * 400;
 	move_and_slide()
+
+func hit():
+	print("damage");
