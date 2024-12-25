a4d1751186342507f2aaaa2eb064eb1101994290
Tamicktom <rickpokemon38@gmail.com>
2024-12-25 11:45:57 -0300

    add text colors

--- scenes/user_interface/ui.gd
+++ scenes/user_interface/ui.gd
@@ -1,5 +1,9 @@
 extends CanvasLayer
 
+# Colors
+var green: Color = Color("6bbfa3");
+var red: Color = Color(0.9, 0, 0, 1);
+
 @onready var laser_label: Label = $LaserCounter/VBoxContainer/Label;
 @onready var granade_label: Label = $GranadeCounter/VBoxContainer/Label;
 
@@ -8,7 +12,18 @@ func _ready() -> void:
 	update_granade_text();
 
 func update_laser_text():
-	laser_label.text = str(Globals.laser_amount);
+	var amount: int = Globals.laser_amount;
+	laser_label.text = str(amount);
+	if amount <= 0:
+		laser_label.modulate = red;
+	else:
+		laser_label.modulate = green;
+	
 
 func update_granade_text():
-	granade_label.text = str(Globals.granade_amount);
+	var amount: int = Globals.granade_amount;
+	granade_label.text = str(amount);
+	if amount <= 0:
+		granade_label.modulate = red;
+	else:
+		granade_label.modulate = green;
