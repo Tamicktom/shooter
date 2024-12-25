5b24a8f6a433e741c28309df45adccb1b4ab91fb
Tamicktom <rickpokemon38@gmail.com>
2024-12-25 11:49:30 -0300

    update colors

--- scenes/user_interface/ui.gd
+++ scenes/user_interface/ui.gd
@@ -5,7 +5,9 @@ var green: Color = Color("6bbfa3");
 var red: Color = Color(0.9, 0, 0, 1);
 
 @onready var laser_label: Label = $LaserCounter/VBoxContainer/Label;
+@onready var laser_icon: TextureRect = $LaserCounter/VBoxContainer/TextureRect;
 @onready var granade_label: Label = $GranadeCounter/VBoxContainer/Label;
+@onready var granade_icon: TextureRect = $GranadeCounter/VBoxContainer/TextureRect;
 
 func _ready() -> void:
 	update_laser_text();
@@ -15,15 +17,19 @@ func update_laser_text():
 	var amount: int = Globals.laser_amount;
 	laser_label.text = str(amount);
 	if amount <= 0:
-		laser_label.modulate = red;
+		update_color(laser_label, laser_icon, red);
 	else:
-		laser_label.modulate = green;
+		update_color(laser_label, laser_icon, green);
 	
 
 func update_granade_text():
 	var amount: int = Globals.granade_amount;
 	granade_label.text = str(amount);
 	if amount <= 0:
-		granade_label.modulate = red;
+		update_color(granade_label, granade_icon, red);
 	else:
-		granade_label.modulate = green;
+		update_color(granade_label, granade_icon, green);
+
+func update_color(label: Label, icon: TextureRect, color: Color) -> void:
+	label.modulate = color;
+	icon.modulate = color;
