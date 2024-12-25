a7d2eb51e19c72a819cc210993958c6ef8479848
Tamicktom <rickpokemon38@gmail.com>
2024-12-25 16:51:30 -0300

    basic

--- scenes/user_interface/transition_layer.gd
+++ scenes/user_interface/transition_layer.gd
@@ -7,5 +7,5 @@ func change_scene(target: String) -> void:
 	$AnimationPlayer.play("fade_to_black");
 	await $AnimationPlayer.animation_finished;
 	get_tree().change_scene_to_file.bind(target).call_deferred();
-	$AnimationPlayer.play("fade_to_transparent");
+	$AnimationPlayer.play_backwards("fade_to_black");
 	await $AnimationPlayer.animation_finished;
